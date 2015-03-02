0ddeb211e40babe24c75fb10f1c61942b79c5428
seventhstar <seventhstar@mail.ru>
2015-03-02 21:50:24 +0300

    leads history

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,10 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, length: { minimum: 6 }, allow_blank: true
 
+  def self.find_version_author(version)
+    find(version.terminator)   
+  end
+
   # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
