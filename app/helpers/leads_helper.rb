73c319097566c7f609824b282502ceb091a42f0d
seventhstar <seventhstar@mail.ru>
2015-03-04 19:43:02 +0300

    lead channel_name

--- app/helpers/leads_helper.rb
+++ app/helpers/leads_helper.rb
@@ -2,5 +2,24 @@ module LeadsHelper
  def find_version_author_name(version)
     user = User.find_version_author(version) 
     user ? user.name : ''
-  end
+ end
+
+ def channel_name(ch_id)
+    if ch_id?
+       channel=Channel.find(ch_id)
+       channel.name 
+    else 
+      res = ""
+    end
+ end
+
+ def status_name(st_id)
+   	if st_id?
+   		status = Status.find(st_id)
+   		status.name
+   	else
+   	res =""	
+   	end
+ end 
+
 end
