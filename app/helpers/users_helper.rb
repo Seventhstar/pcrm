0cd26ede4db74d0eeed52701bfac7272c54f5046
seventhstar <seventhstar@mail.ru>
2015-04-20 20:34:15 +0300

    birthday

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -10,21 +10,48 @@ module UsersHelper
 
 
   def date_of_birth(user)
+      
+
       if user.date_birth
          a = content_tag("span",user.date_birth.strftime("%d.%m.%Y"))
 
-         $interval = $Date.now->diff(user.date_birth);
-          
+        today = Date.today()         
+        today.change(year: user.date_birth.year)
+        diff = (Date.parse(user.date_birth.to_s)- today).to_i 
+        diff=diff-1
+        #diff = diff/ (24 * 3600)
+        
+        puts "diff"
+        puts diff
+        puts today
+        puts user.date_birth
+        puts Date.parse(user.date_birth.to_s)
+        #s
 
-        d = content_tag :span,"через 3 дня"
-        e = content_tag("span","",{:class=>'icon icon_birthday'})
-         #c = content_tag("span","123",{:class=>'red message'})
-         c = content_tag :span, {:class => 'red message'} do
-            d + e
-             
-         end
-         
-	       a  + c
+        case diff
+        when -1
+          txt = "сегодня!!!"
+        when 0
+          txt = "завтра!"
+        when 1
+          txt = "послезавтра!"  
+        when 2..4
+          txt = "через #{diff} дня"
+        else
+          txt = "через #{diff} дней"
+        end
+              
+
+         if diff <8 && diff >=-1 
+            d = content_tag :span,txt
+            e = content_tag("span","",{:class=>'icon icon_birthday'})
+            c = content_tag :span, {:class => 'red message'} do
+              d + e
+            end
+	          a  + c
+          else
+            a
+         end 
       end
   end
 
